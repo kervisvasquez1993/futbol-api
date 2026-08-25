0e5ecb1e9422bef0b43a8b97a8c9d6ec71d0e083
kervisvasquez1993 <kervisvasquez24@gmail.com>
2026-08-25 19:43:28 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,16 @@ import { TransformInterceptor } from './shared/interceptors/transform.intercepto
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+app.enableCors({
+  origin: (origin, callback) => {
+    if (!origin || origin.endsWith('.vercel.app') || origin === 'https://futbol-frotend.vercel.app') {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido por CORS'));
+    }
+  },
+  credentials: true,
+});
 
   app.useGlobalPipes(
     new ValidationPipe({
